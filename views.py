03f02a8d1b144cb7fccdbf1d4011cafbdfbe941e
Nelestya <tristand.pro@gmail.com>
2017-10-02 15:26:45 +0200

    view modification for run with baseapp

--- views.py
+++ views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render , get_object_or_404
 from django.views import View
 from .models import Category, Product
 from cart.forms import CartAddProductForm
+from baseapp.models import Application
 # Create your views here.
 class Product_list(View):
 
@@ -25,6 +26,8 @@ class Product_detail(View):
 
     def get(self, request, id, slug):
         product = get_object_or_404(Product, id=id, slug=slug, available=True)
+        applications = Application.objects.all()
         cart_product_form = CartAddProductForm()
         return render(request, 'shop/product/detail.html', {'product': product,
-                                                            'cart_product_form': cart_product_form})
+                                                            'cart_product_form': cart_product_form,
+                                                            'applications': applications})
